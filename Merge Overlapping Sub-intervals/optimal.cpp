41a91f577b270aee0300d023bf9ab1f635ce5504
partht_12 <tambeparth27@gmail.com>
2025-04-05 23:06:15 +0530

    minor changes

--- Merge Overlapping Sub-intervals/optimal.cpp
+++ Merge Overlapping Sub-intervals/optimal.cpp
@@ -14,14 +14,12 @@ vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
 
     for (int i = 0; i < n; i++)
     {
-        // if the current interval does not
-        // lie in the last interval:
+        // if the current interval does not lie in the last interval:
         if (ans.empty() || arr[i][0] > ans.back()[1])
         {
             ans.push_back(arr[i]);
         }
-        // if the current interval
-        // lies in the last interval:
+        // if the current interval lies in the last interval:
         else
         {
             ans.back()[1] = max(ans.back()[1], arr[i][1]);
