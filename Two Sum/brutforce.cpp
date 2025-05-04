74aa97e2a563565f21559a2e7830fd6d137f020d
partht_12 <tambeparth27@gmail.com>
2025-05-05 00:20:22 +0530

    modifed code

--- Two Sum/brutforce.cpp
+++ Two Sum/brutforce.cpp
@@ -1,27 +1,55 @@
-#include <iostream>
-#include <vector>
+
+#include <bits/stdc++.h>
 using namespace std;
-bool twoSum(vector<int> &arr, int tar)
+
+vector<int> twoSum(vector<int> &arr, int target)
 {
+    vector<int> ans;
     int n = arr.size();
     for (int i = 0; i < n; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[i] + arr[j] == tar)
+            if (arr[i] + arr[j] == target)
             {
-                return true;
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
             }
         }
     }
-    return false;
+    return {-1, -1};
 }
+// bool twoSum(vector<int> &arr, int tar)
+// {
+//     int n = arr.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[i] + arr[j] == tar)
+//             {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
 
 int main()
 {
     vector<int> arr = {2, 6, 5, 8, 11};
-    int tar = 15;
-    bool ans = twoSum(arr, tar);
-    cout << boolalpha << ans;
+    int target = 16;
+    vector<int> ans = twoSum(arr, target);
+
+    if (ans[0] == -1)
+    {
+        cout << "No valid pair found." << endl;
+    }
+    else
+    {
+        cout << "Pair is: {" << arr[ans[0]] << ", " << arr[ans[1]] << "}" << endl;
+    }
+
     return 0;
 }
