60f53a4e8256ddb5919609aee8e6fd0db3a5b351
partht_12 <tambeparth27@gmail.com>
2025-05-17 23:35:32 +0530

    brut and optimal

--- Length of the longest subarray with zero Sum/brut.cpp
+++ Length of the longest subarray with zero Sum/brut.cpp
@@ -1,27 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> &a)
+int bruteForce(vector<int> &a)
 {
+    int n = a.size();
     int maxLen = 0;
-    unordered_map<int, int> sumIndexMap;
-    int sum = 0;
 
-    for (int i = 0; i < a.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        sum += a[i];
+        int sum = 0;
 
-        if (sum == 0)
+        for (int j = i; j < n; j++)
         {
-            maxLen = i + 1;
-        }
-        else if (sumIndexMap.find(sum) != sumIndexMap.end())
-        {
-            maxLen = max(maxLen, i - sumIndexMap[sum]);
-        }
-        else
-        {
-            sumIndexMap[sum] = i;
+            sum += a[j];
+
+            if (sum == 0)
+            {
+                maxLen = max(maxLen, j - i + 1);
+            }
         }
     }
 
@@ -31,7 +27,6 @@ int solve(vector<int> &a)
 int main()
 {
     vector<int> a = {9, -3, 3, -1, 6, -5};
-    cout << solve(a) << endl;
-
+    cout << bruteForce(a) << endl; // Output: 5
     return 0;
 }
