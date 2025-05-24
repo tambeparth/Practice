cebc3b3087cb012921ebaf32e63ef37c8fcddd18
partht_12 <tambeparth27@gmail.com>
2025-05-24 23:36:16 +0530

    few changes

--- Reverse Linked Lis/optimal_one.cpp
+++ Reverse Linked Lis/optimal_one.cpp
@@ -4,28 +4,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-// Node class represents a
-// node in a linked list
 class Node
 {
 public:
-    // Data stored in the node
     int data;
-
-    // Pointer to the next node in the list
     Node *next;
 
-    // Constructor with both data and
-    // next node as parameters
     Node(int data1, Node *next1)
     {
         data = data1;
         next = next1;
     }
 
-    // Constructor with only data as a
-    // parameter, sets next to nullptr
     Node(int data1)
     {
         data = data1;
