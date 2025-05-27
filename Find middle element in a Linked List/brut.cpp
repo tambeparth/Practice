7bdede3cafaf33d06695b0266aeaf57db852aba6
partht_12 <tambeparth27@gmail.com>
2025-05-27 23:37:56 +0530

    few changes

--- Find middle element in a Linked List/brut.cpp
+++ Find middle element in a Linked List/brut.cpp
@@ -4,26 +4,19 @@
 
 using namespace std;
 
-// Node class represents a node in a linked list
 class Node
 {
 public:
-    // Data stored in the node
     int data;
 
-    // Pointer to the next node in the list
     Node *next;
 
-    // Constructor with both data
-    // and next node as parameters
     Node(int data1, Node *next1)
     {
         data = data1;
         next = next1;
     }
 
-    // Constructor with only data as
-    // a parameter, sets next to nullptr
     Node(int data1)
     {
         data = data1;
@@ -31,8 +24,6 @@ public:
     }
 };
 
-// Function to find the middle
-// node of a linked list
 Node *findMiddle(Node *head)
 {
     // If the list is empty or has only
