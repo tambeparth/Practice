474ac0318b24b12dd92a663afb28060925b51e97
partht_12 <tambeparth27@gmail.com>
2025-05-28 16:37:59 +0530

    checking

--- Find middle element in a Linked List/brut.cpp
+++ Find middle element in a Linked List/brut.cpp
@@ -26,9 +26,7 @@ public:
 
 Node *findMiddle(Node *head)
 {
-    // If the list is empty or has only
-    // one element, return the head as
-    // it's the middle.
+
     if (head == NULL || head->next == NULL)
     {
         return head;
@@ -37,42 +35,34 @@ Node *findMiddle(Node *head)
     Node *temp = head;
     int count = 0;
 
-    // Count the number of nodes in the linked list.
     while (temp != NULL)
     {
         count++;
         temp = temp->next;
     }
 
-    // Calculate the position of the middle node.
     int mid = count / 2 + 1;
     temp = head;
 
-    // Traverse to the middle node by moving
-    // temp to the middle position.
     while (temp != NULL)
     {
         mid = mid - 1;
 
-        // Check if the middle
-        // position is reached.
         if (mid == 0)
         {
-            // break out of the loop
-            // to return temp
+
             break;
         }
-        // Move temp ahead
+
         temp = temp->next;
     }
 
-    // Return the middle node.
     return temp;
 }
 
 int main()
 {
-    // Creating a sample linked list:
+
     Node *head = new Node(1);
     head->next = new Node(2);
     head->next->next = new Node(3);
@@ -80,10 +70,8 @@ int main()
     head->next->next->next->next = new Node(5);
     head->next->next->next->next->next = new Node(6);
 
-    // Find the middle node
     Node *middleNode = findMiddle(head);
 
-    // Display the value of the middle node
     cout << "The middle node value is: " << middleNode->data << endl;
 
     return 0;
