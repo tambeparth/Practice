c0db7f6067b8b7b053e0e0fb487d22cb02892b0f
partht_12 <tambeparth27@gmail.com>
2025-06-19 23:50:51 +0530

    brut approach with Time Complexity: O(N * 2 * log(N) )

---
+++ Detect a Cycle in a Linked List/brut.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Node class represents a
+// node in a linked list
+class Node
+{
+public:
+    // Data stored in the node
+    int data;
+
+    // Pointer to the next node in the list
+    Node *next;
+
+    // Constructor with both data
+    // and next node as parameters
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor with only data as
+    // a parameter, sets next to nullptr
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+// function to detect loop in linked list
+bool detectLoop(Node *head)
+{
+
+    // Initialize a pointer 'temp'
+    // at the head of the linked list
+    Node *temp = head;
+
+    // Create a map to keep track of
+    // encountered nodes
+    unordered_map<Node *, int> nodeMap;
+
+    // Step 2: Traverse the linked list
+    while (temp != nullptr)
+    {
+        // If the node is already in the
+        // map, there is a loop
+        if (nodeMap.find(temp) != nodeMap.end())
+        {
+            return true;
+        }
+        // Store the current node
+        // in the map
+        nodeMap[temp] = 1;
+
+        // Move to the next node
+        temp = temp->next;
+    }
+
+    // Step 3: If the list is successfully traversed
+    // without a loop, return false
+    return false;
+}
+
+int main()
+{
+    // Create a sample linked list
+    // with a loop for testing
+
+    Node *head = new Node(1);
+    Node *second = new Node(2);
+    Node *third = new Node(3);
+    Node *fourth = new Node(4);
+    Node *fifth = new Node(5);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    // Create a loop
+    fifth->next = third;
+
+    // Check if there is a loop
+    // n the linked list
+    if (detectLoop(head))
+    {
+        cout << "Loop detected in the linked list." << endl;
+    }
+    else
+    {
+        cout << "No loop detected in the linked list." << endl;
+    }
+
+    // Clean up memory (free the allocated nodes)
+    delete head;
+    delete second;
+    delete third;
+    delete fourth;
+    delete fifth;
+
+    return 0;
+}
